0be1bd628090254b45ac44cefe0a2cd3942e4bae
lawkelvin33 <lawkelvin33@korea.ac.kr>
2023-06-04 02:49:25 +0900

    dependencies resolved

--- src/ex-apis/ex-apis.module.ts
+++ src/ex-apis/ex-apis.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ExApisService } from './ex-apis.service';
 import { ExApisController } from './ex-apis.controller';
+import { PrismaService } from 'src/common/services/prisma.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  providers: [ExApisService],
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
+  providers: [ExApisService, PrismaService],
   controllers: [ExApisController],
 })
 export class ExApisModule {}
